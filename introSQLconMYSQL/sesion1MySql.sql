472411e65218e7b86f2df3322383fcf7da59850c
Ximena Gallego <mxgallego@unicauca.edu.co>
2023-05-22 14:34:34 -0500

    introducción a SQL con MYSQL: manipulación y consultar datos

---
+++ introSQLconMYSQL/sesion1MySql.sql
@@ -0,0 +1,8 @@
+#usar base de datos
+USE world;
+#selecionar la tabal city
+select * from city;
+#selecionar la tabal country
+SELECT * FROM country;
+#selecionar la tabal countrylanguage
+SELECT * FROM countrylanguage;
---
+++ introSQLconMYSQL/sesion2MySql.sql
@@ -0,0 +1,8 @@
+#Crear base de datos
+CREATE DATABASE jugos;
+#Eliminar base de datos
+DROP SCHEMA jugos;
+#selecionar información de la tabla City
+SELECT * FROM CITY;
+#selecionar información de la tabla Country
+select * from country;
